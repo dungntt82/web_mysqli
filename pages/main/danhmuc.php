0f4f9e5325a3e4fbf162a8f2b894653bdbf506cc
Nguyễn Thị Thùy Dung <4201103013@student.hcmue.edu.vn>
2023-07-25 18:55:33 +0700

    007

--- pages/main/danhmuc.php
+++ pages/main/danhmuc.php
@@ -1,3 +1,6 @@
+<php
+    
+?>
 <h5>Danh mục sản phẩm</h5>
 <ul class="product-list">
     <a href="#">
@@ -7,53 +10,5 @@
             <p class="price-product">Giá: 320000</p>
         </li>
     </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
-    <a href="#">
-        <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
-        </li>
-    </a>
+  
 </ul>
