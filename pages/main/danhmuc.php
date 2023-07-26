eaa6554f8ae623f00dfd7b43a3ce8a4be58ba915
Nguyễn Thị Thùy Dung <4201103013@student.hcmue.edu.vn>
2023-07-26 20:25:20 +0700

    008

--- pages/main/danhmuc.php
+++ pages/main/danhmuc.php
@@ -1,14 +1,23 @@
-<php
-    
+<?php
+$sql_pro = "SELECT * FROM tbl_danhmuc, tbl_sanpham WHERE tbl_sanpham.id_danhmuc = tbl_danhmuc.id_danhmuc AND tbl_sanpham.id_danhmuc = '$_GET[id]' ORDER BY tbl_sanpham.id_sanpham DESC";
+$query_pro = mysqli_query($mysqli, $sql_danhmuc);
+$row_title = mysqli_fetch_array($query_pro);
+
+
 ?>
-<h5>Danh mục sản phẩm</h5>
+<h5>Danh mục sản phẩm: <?php echo $row_title['tendanhmuc'] ?></h5>
 <ul class="product-list">
-    <a href="#">
+    <?php
+    while ($row_pro = mysqli_fetch_array($query_pro)) {
+    ?>
         <li>
-            <img src="images/tainghe001.png" alt="">
-            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-            <p class="price-product">Giá: 320000</p>
+            <a href="#">
+                <img src="images/tainghe001.png" alt="">
+                <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
+                <p class="price-product">Giá: 320000</p>
+            </a>
         </li>
-    </a>
-  
+    <?php
+    }
+    ?>
 </ul>
