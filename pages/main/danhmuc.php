1546afd46106370e25bd3869d65ecc17bb77b87e
Nguyễn Thị Thùy Dung <4201103013@student.hcmue.edu.vn>
2023-07-28 20:57:01 +0700

    010

--- pages/main/danhmuc.php
+++ pages/main/danhmuc.php
@@ -1,8 +1,10 @@
 <?php
-$sql_pro = "SELECT * FROM tbl_danhmuc, tbl_sanpham WHERE tbl_sanpham.id_danhmuc = tbl_danhmuc.id_danhmuc AND tbl_sanpham.id_danhmuc = '$_GET[id]' ORDER BY tbl_sanpham.id_sanpham DESC";
-$query_pro = mysqli_query($mysqli, $sql_danhmuc);
-$row_title = mysqli_fetch_array($query_pro);
+$sql_pro = "SELECT * FROM tbl_sanpham WHERE id_danhmuc = '$_GET[id]' ORDER BY id_sanpham DESC";
+$query_pro = mysqli_query($mysqli, $sql_pro);
 
+$sql_cate = "SELECT * FROM tbl_danhmuc WHERE id_danhmuc = '$_GET[id]' ORDER BY id_danhmuc LIMIT 1";
+$query_cate = mysqli_query($mysqli, $sql_cate);
+$row_title = mysqli_fetch_array($query_cate);
 
 ?>
 <h5>Danh mục sản phẩm: <?php echo $row_title['tendanhmuc'] ?></h5>
@@ -11,11 +13,11 @@ $row_title = mysqli_fetch_array($query_pro);
     while ($row_pro = mysqli_fetch_array($query_pro)) {
     ?>
         <li>
-            <!-- <a href="#"> -->
-                <img src="admincp/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>" alt="">
-                <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
-                <p class="price-product">Giá: 320000</p>
-            <!-- </a> -->
+            <a href="#">
+                <img src="admincp/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>">
+                <p class="title-product">Tên sản phẩm: <?php echo $row_pro['tensanpham'] ?></p>
+                <p class="price-product">Giá: <?php echo number_format($row_pro['giasp'],0,',','.').'vnđ' ?></p>
+            </a>
         </li>
     <?php
     }
