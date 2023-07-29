606714bd845053453a922d89c1a1d3f2acc286be
Nguyễn Thị Thùy Dung <4201103013@student.hcmue.edu.vn>
2023-07-29 21:37:07 +0700

    013

--- pages/main/sanpham.php
+++ pages/main/sanpham.php
@@ -8,15 +8,18 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
         <div class="hinhanh_sanpham">
             <img src="admincp/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh'] ?>" alt="">
         </div>
-
-        <div class="chitiet_sanpham">
-            <h3>Tên sản phẩm: <?php echo $row_chitiet['tensanpham'] ?></h3>
-            <h3>Mã sản phẩm: <?php echo $row_chitiet['masp'] ?></h3>
-            <h3>Giá sản phẩm: <?php echo number_format($row_chitiet['giasp'], 0, ',', '.') . ' vnđ' ?></h3>
-            <h5>Tóm tắt: <?php echo $row_chitiet['tomtat'] ?></h5>
-            <p>Nội dung: <?php echo $row_chitiet['noidung'] ?></p>
-        </div>
+        <form method="POST" action="pages/main/themgiohang.php?idsanpham=<?php echo $row_chitiet['id_sanpham']?>">
+            <div class="chitiet_sanpham">
+                <h3>Tên sản phẩm: <?php echo $row_chitiet['tensanpham'] ?></h3>
+                <h3>Mã sản phẩm: <?php echo $row_chitiet['masp'] ?></h3>
+                <h3>Giá sản phẩm: <?php echo number_format($row_chitiet['giasp'], 0, ',', '.') . ' vnđ' ?></h3>
+                <h5>Tóm tắt: <?php echo $row_chitiet['tomtat'] ?></h5>
+                <p>Nội dung: <?php echo $row_chitiet['noidung'] ?></p>
+                <p><input class="btnthemgiohang" type="submit" value="THÊM GIỎ HÀNG" name="themgiohang"></p>
+            </div>
+        </form>
     </div>
+
 <?php
 }
 ?>
---
+++ pages/main/themgiohang.php
@@ -0,0 +1,38 @@
+<p>THÊM GIỎ HÀNG</p>
+<?php
+session_start();
+include('../../admincp/config/config.php');
+if (isset($_POST['themgiohang'])) {
+    // session_destroy()
+    $id = $_GET['idsanpham'];
+    $soluong = 1;
+    $sql = "SELECT * FROM tbl_sanpham WHERE id_sanpham ='" . $id . "' LIMIT 1";
+    $query = mysqli_query($mysqli, $sql);
+    $row =  mysqli_fetch_array($query);
+    if ($row) {
+        $new_product = array(array('tensanpham' => $row['tensanpham'], 'soluong' => $soluong, 'id' => $id, 'giasp' => $row['giasp'], 'hinhanh' => $row['hinhanh'], 'masp' => $row['masp']));
+        //kiểm tra session giỏ hàng tồn tại hay chưa.
+        if (isset($_SESSION['cart'])) {
+            $found = false;
+            foreach ($_SESSION['cart'] as $cart_item) {
+                # code...
+                if ($cart_item['id'] == $id) {
+                    $product = array(array('tensanpham' => $cart_item['tensanpham'],  'soluong' => $soluong+1, 'id' => $cart_item['id'], 'giasp' => $cart_item['giasp'], 'hinhanh' => $cart_item['hinhanh'], 'masp' => $cart_item['masp']));
+                    $found = true;
+                } else {
+                    $product = array(array('tensanpham' => $cart_item['tensanpham'],  'soluong' => $soluong, 'id' => $cart_item['id'], 'giasp' => $cart_item['giasp'], 'hinhanh' => $cart_item['hinhanh'], 'masp' => $cart_item['masp']));
+                }
+            }
+            if($found==false){
+                $_SESSION['cart'] = array_merge($product,$new_product);
+            }
+            else{
+                $_SESSION['cart'] = $new_product;
+            }
+        } else {
+            $_SESSION['cart'] = $new_product;
+        }
+    }
+    header('location:../../index.php?quanly=giohang');
+}
+?>
